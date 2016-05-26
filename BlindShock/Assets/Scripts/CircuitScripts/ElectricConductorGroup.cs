1384e2405623e7e3ba99f0873d963d688a3a05d1
zlurker <tallspin_97@hotmail.com>
2016-05-26 15:31:58 +0800

    Circuit
    Circuit implemented for room 1 and 2.

---
+++ BlindShock/Assets/Scripts/CircuitScripts/ElectricConductorGroup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricConductorGroup : ElectricConductor {
+
+    public override void VoltageFlow() {
+
+        foreach (Transform subWires in transform) {
+            subWires.GetComponent<Renderer>().material.color = charge > 0 ? Color.yellow : Color.white;
+        }
+
+        if (predecessorWire != null) {
+            predecessorWire.charge += chargeValue;
+            predecessorWire.chargeValue = chargeValue;
+            //predecessorWire.chargeTravelTime = chargeTravelTime;
+            predecessorWire.VoltageFlow();
+        }
+    }
+}
---
+++ BlindShock/Assets/Scripts/CircuitScripts/EletricDeconductor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class EletricDeconductor : MonoBehaviour {
+
+    ElectricConductor storage;
+
+    void OnCollisionEnter(Collision collision) {
+        if (storage == null) {
+            if (collision.transform.root.gameObject.GetComponent<ElectricConductor>()) {
+                storage = collision.transform.root.gameObject.GetComponent<ElectricConductor>();
+                storage.chargeValue = -storage.charge;
+                storage.VoltageFlow();
+
+                if (storage.GetComponent<ElectricConductorGroup>()) {
+                    foreach (Transform multipleWires in storage.transform) {
+                        multipleWires.GetComponent<Renderer>().material.color = Color.white;
+                    }
+                } else {
+                    storage.GetComponent<Renderer>().material.color = Color.white;
+                }
+                storage.predecessorWire = null;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (storage != null) {
+            storage.chargeValue = storage.charge;
+            storage.predecessorWire = storage.storageWire;
+            storage.VoltageFlow();
+            storage = null;
+        }
+    }
+}
