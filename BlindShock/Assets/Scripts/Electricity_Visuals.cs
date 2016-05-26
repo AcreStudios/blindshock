f5aa3b21ee2ed8dfb64eee6eaa00e03f29cf1241
Aweife <aweifeisaweife@gmail.com>
2016-05-26 18:03:58 +0800

    Created a lightning prefab
    Assign start + end transform to use the lightning! Duplicate it AFTER
    assign start and end to increase intensity.

---
+++ BlindShock/Assets/Scripts/Electricity_Visuals.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Electricity_Visuals : MonoBehaviour 
+{
+	#region Variables
+	public int numberOfSegments = 12;
+	public float randomPositionOffset = 0.2f;
+
+	public float lifetimeDuration = 1f;
+	Color aColor = Color.white;
+	public float decayRate = 5f; // Fade away rate after lifetime duration is up
+
+	// Set by other scripts
+	public Transform startPos;
+	public Transform endPos;
+	#endregion
+
+	#region Cache components
+	LineRenderer lineRend;
+	#endregion
+
+	void Awake()
+	{
+		// Cache component
+		lineRend = GetComponent<LineRenderer>();
+
+		// Set material
+		// We can have an static material bank later...
+	}
+
+	void Start() 
+	{
+		// How many segments the electricity will have
+		lineRend.SetVertexCount(numberOfSegments);
+
+		// Creates that zig-zag shape
+		RandomizePositions();
+	}
+	
+	void RandomizePositions()
+	{
+		for(int i = 0; i < numberOfSegments; i++)
+		{
+			float x = Mathf.Lerp(startPos.position.x, endPos.position.x, i / (numberOfSegments - 1f)) + Random.Range(-randomPositionOffset, randomPositionOffset);
+			float y = Mathf.Lerp(startPos.position.y, endPos.position.y, i / (numberOfSegments - 1f)) + Random.Range(-randomPositionOffset, randomPositionOffset);
+			float z = Mathf.Lerp(startPos.position.z, endPos.position.z, i / (numberOfSegments - 1f)) + Random.Range(-randomPositionOffset, randomPositionOffset);
+
+			lineRend.SetPosition(i, new Vector3(x, y, z));
+		}
+
+		StartCoroutine(RebirthSelf());
+	}
+
+	IEnumerator RebirthSelf()
+	{
+		yield return new WaitForSeconds(lifetimeDuration);
+		while(aColor.a > 0)
+		{
+			aColor.a -= decayRate * Time.deltaTime;
+			lineRend.SetColors(aColor, aColor);
+
+			yield return null;
+		}
+
+		RandomizePositions();
+		aColor = Color.white;
+		lineRend.SetColors(aColor, aColor);
+	}
+}
